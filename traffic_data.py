e30d9446a53e88ce68ce76ab101e5e98189a16fd
WireNext <wirennextt@gmail.com>
2025-06-09 20:14:26 +0200

    Update traffic_data.py

--- traffic_data.py
+++ traffic_data.py
@@ -108,8 +108,8 @@ def process_xml_from_url(url, region_name, all_incidents):
                 # Extraer aviso
                 lane = situation_record.find(".//_0:networkManagementType", NS)
                 if lane is not None and lane.text:
-                    translated = translate_incident_type(lane.text)
-                    description.append(f"<b>Aviso: </b> {translated}")
+                    warning_translated = translate_incident_type(lane.text)
+                    description.append(f"<b>Aviso: </b> {warning_translated}")
 
                 # Extraer impacto
                 impact = situation_record.find(".//_0:impactOnTraffic", NS)
